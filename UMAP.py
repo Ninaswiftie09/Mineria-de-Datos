b22adb140f1ed1c285f2601108a3f239d77c8e0e
Mishell Jiménez <cip231169@uvg.edu.gt>
2026-02-20 21:28:13 -0600

    UMAP

--- UMAP.py
+++ UMAP.py
@@ -1,35 +1,32 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_breast_cancer
 from sklearn.preprocessing import StandardScaler
-from sklearn.manifold import TSNE
+import umap
 
-# 1. Cargar dataset
+# Cargar dataset
 data = load_breast_cancer()
 X = data.data
 y = data.target
 
-# 2. Estandarizar (MUY IMPORTANTE)
+# Estandarizar
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
-# 3. Aplicar t-SNE
-tsne = TSNE(
+# Aplicar UMAP
+reducer = umap.UMAP(
     n_components=2,
-    perplexity=30,
-    learning_rate=200,
-    max_iter=1000,
+    n_neighbors=15,
+    min_dist=0.1,
     random_state=42
 )
 
+X_umap = reducer.fit_transform(X_scaled)
 
-X_embedded = tsne.fit_transform(X_scaled)
-
-# 4. Graficar
+# Graficar
 plt.figure(figsize=(8,6))
-scatter = plt.scatter(X_embedded[:,0], X_embedded[:,1], c=y)
-plt.title("t-SNE - Breast Cancer Dataset")
+scatter = plt.scatter(X_umap[:,0], X_umap[:,1], c=y)
+plt.title("UMAP - Breast Cancer Dataset")
 plt.xlabel("Componente 1")
 plt.ylabel("Componente 2")
 plt.colorbar(scatter)
