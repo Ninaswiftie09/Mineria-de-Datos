d4f87603136769fae057d48378aef2873f7fd0d2
Ninaswiftie09 <naj231088@uvg.edu.gt>
2026-02-23 17:05:37 -0600

    archivo donde vamos a trabajar

---
+++ Clustering.py
@@ -0,0 +1,11 @@
+# 1.1 Pre procesamiento 
+
+# 1.2 Tendencia al agrupamiento
+
+# 1.3 Número de clústeres
+
+# 1.4 Aplicar 2 algoritmos: K-means, Clustering jerárquico, Comparación entre ambos
+
+# 1.5 Calidad del agrupamiento
+
+# 1.6 Interpretación de grupos
